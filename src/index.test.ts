39162181683394528319d63992e5ceca8f5c6d82
Daniel Nixon <dan.nixon@gmail.com>
2020-07-22 20:42:25 +1000

    Update dependencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
 /* eslint-disable jest/no-identical-title */
 /* eslint-disable sonarjs/no-identical-functions */
 /* eslint-disable functional/no-return-void */
