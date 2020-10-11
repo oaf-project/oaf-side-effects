97b77fc3b6039310d1ccee6d91b4561925686675
Daniel Nixon <dan.nixon@gmail.com>
2020-10-11 22:25:04 +1100

    Update dependencies

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,12 @@
-module.exports = function(config) {
-  config.set({
-    mutator: "typescript",
-    packageManager: "yarn",
-    reporters: ["clear-text", "progress"],
-    testRunner: "jest",
-    transpilers: [],
-    coverageAnalysis: "off",
-    tsconfigFile: "tsconfig.json",
-    mutate: ["src/**/*.ts", "!src/**/*.test.ts"],
-    thresholds: { high: 80, low: 60, break: 30 }
-  });
+module.exports = {
+  packageManager: "yarn",
+  reporters: ["clear-text", "progress"],
+  testRunner: "jest",
+  // TODO: https://github.com/stryker-mutator/stryker/issues/2316
+  coverageAnalysis: "off",
+  // TODO
+  // checkers: ["typescript"],
+  tsconfigFile: "tsconfig.json",
+  mutate: ["src/**/*.ts", "!src/**/*.test.ts"],
+  thresholds: { high: 80, low: 60, break: 30 }
 };
