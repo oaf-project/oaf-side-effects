77b423233536de2bda3fb88749940969149edee3
Daniel Nixon <dan.nixon@gmail.com>
2021-08-29 17:46:42 +1000

    Update stryker.conf.js

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,11 +1,9 @@
 module.exports = {
   packageManager: "yarn",
-  reporters: ["clear-text", "progress"],
+  reporters: ["clear-text", "progress", "dashboard"],
   testRunner: "jest",
-  // TODO: https://github.com/stryker-mutator/stryker/issues/2316
-  coverageAnalysis: "off",
-  // TODO
-  // checkers: ["typescript"],
+  coverageAnalysis: "perTest",
+  checkers: ["typescript"],
   tsconfigFile: "tsconfig.json",
   mutate: ["src/**/*.ts", "!src/**/*.test.ts"],
   thresholds: { high: 80, low: 60, break: 30 }
